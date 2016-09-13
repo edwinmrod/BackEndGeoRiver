18f89f84a2f66c8111a227bd90ee419b27c9ac19
edwinmrod <emrodriguezr@unbosque.edu.co>
2016-09-12 23:07:32 -0500

    migration Users

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\users;
+use Illuminate\Http\Request;
+use Session;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $users = users::paginate(25);
+
+        return view('API.users.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('API.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        
+        $requestData = $request->all();
+        
+        users::create($requestData);
+
+        Session::flash('flash_message', 'users added!');
+
+        return redirect('API/users');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $user = users::findOrFail($id);
+
+        return view('API.users.show', compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $user = users::findOrFail($id);
+
+        return view('API.users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update($id, Request $request)
+    {
+        
+        $requestData = $request->all();
+        
+        $user = users::findOrFail($id);
+        $user->update($requestData);
+
+        Session::flash('flash_message', 'users updated!');
+
+        return redirect('API/users');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function destroy($id)
+    {
+        users::destroy($id);
+
+        Session::flash('flash_message', 'users deleted!');
+
+        return redirect('API/users');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('firstName');
+			$table->string('lastName');
+            $table->string('userName')->unique();
+            $table->string('password', 60);
+			$table->string('program', 60);
+            $table->int('role', 1);
         });
     }
 
